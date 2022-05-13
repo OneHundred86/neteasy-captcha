9b10541640693d4ec933a6f47dd471f1883ce44f
chenguangjun <chen.gj@qq.com>
2022-05-13 14:09:08 +0800

    README

--- README.md
+++ README.md
@@ -35,5 +35,5 @@ composer require "oh86/netesay-captcha"
 4. 校验验证码
    ```php
    use Oh86\NetEasy\Captcha\Facade\CaptchaVerify;
-   CaptchaVerify::verify('xxx');
+   CaptchaVerify::verify('CN31_26SIlYVCJAEkcFMPU_5dJbZQHNwnYAbdLBUV8-DuPFY_rxecR.mfDufJdy5dP0P-uBUdwy4fYwwMLhGqJRunbr6H-yoTwXey9MRjZ81buapfcg8qwhzaanpufToTi-KCGq0AeSiA9dN7syixFUFmnMUi8Br870YJqNSmlzrrKRVWlhHGTmhUX9BPV5EqCyg4nB6eBPdpG5T4TV00s_uyTnbuBkUBhWqK-7DM8hwJ-ea.16of7X99yQvYG8vPmnFT9vM5QjKu5aKAYGDwS9wL.o8d88XVBum06tWiRujnCjIBT7rko2dhVfen6bUFbGefyyhiCL7NLJ_sdx_qU76UZ0vPAYkZtFFEVRKHtNAxHI_lqvPG4mWqOU1ftK6v7_UFMQkc4Gw5H5WIFAwwwViKlCAQHedutYYUUCAF8zp5SCMh1dI77NLiJ6sUlo1qOe1anz6mFmgJYczKscJeeKsVz6lBm5nlgaGUjgPNBhWkLBTA6v79iH4gTE_GHWN3');
    ```
--- src/CaptchaProvider.php
+++ src/CaptchaProvider.php
@@ -5,9 +5,23 @@ namespace Oh86\NetEasy\Captcha;
 
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\Application as LaravelApplication;
 
 class CaptchaProvider extends ServiceProvider
 {
+    /**
+     * Boot the service provider.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $source = realpath($raw = __DIR__.'/../config/neteasy-captcha.php') ?: $raw;
+        if ($this->app instanceof LaravelApplication && $this->app->runningInConsole()) {
+            $this->publishes([$source => config_path('neteasy-captcha.php')]);
+        }
+    }
+
     /**
      * Register any application services.
      *
@@ -16,7 +30,7 @@ class CaptchaProvider extends ServiceProvider
     public function register()
     {
         $this->app->singleton('neteasy_captcha_verify', function($app) {
-            $config = $app->get('config')['neteasy']['captcha'];
+            $config = $app->get('config')['neteasy-captcha'];
             return new CaptchaVerify($config['secret_id'], $config['secret_key'], $config['captcha_id']);
         });
     }
